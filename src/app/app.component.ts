d05b1d66f3129174ba1f278150c4f4e194b577ac
HLong <hailongkc1203@gmail.com>
2024-06-05 15:30:44 +0700

    Angular Content Child Decorator - In-Depth Explanation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,31 @@
-import { AfterViewInit, Component, ElementRef, QueryList, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { COURSES } from './db-data';
 import { Course } from './model/course';
 import { NgIf } from '@angular/common';
+import { CourseImageComponent } from './course-image/course-image.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CourseCardComponent, NgIf],
+  imports: [RouterOutlet, CourseCardComponent, NgIf, CourseImageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements AfterViewInit {
 
   courses = [...COURSES];
+
+  @ViewChildren(CourseCardComponent, { read: ElementRef })
+  cards!: QueryList<ElementRef>
+
   constructor() {
 
   }
 
   ngAfterViewInit(): void {
+    console.log(this.cards);
 
   }
   onCourseSelected(course: Course) {
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Course } from '../model/course';
 import { NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
+import { CourseImageComponent } from '../course-image/course-image.component';
 
 @Component({
   selector: 'app-course-card',
@@ -9,15 +10,17 @@ import { NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
-export class CourseCardComponent implements OnInit {
+export class CourseCardComponent implements OnInit, AfterViewInit {
 
   @Input()
   course!: Course;
 
-
-
   @Output('courseSelected')
   courseSelected = new EventEmitter<Course>();
+
+  @ContentChild(CourseImageComponent, { read: ElementRef })
+  image!: ElementRef;
+
 
   ngOnInit(): void {
 
@@ -25,6 +28,10 @@ export class CourseCardComponent implements OnInit {
 
   constructor() { }
 
+  ngAfterViewInit(): void {
+    console.log(this.image);
+
+  }
   onCourseViewed() {
     this.courseSelected.emit(this.course)
   }
